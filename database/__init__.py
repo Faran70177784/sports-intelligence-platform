cf40cc4bf3d90b535eb28040f49904b31d921bce
Faran70177784 <70177784@student.uol.edu.pk>
2026-07-03 06:32:22 +0500

    Add SQLAlchemy database foundation

---
+++ database/__init__.py
@@ -0,0 +1,4 @@
+from .database import Base
+from .database import SessionLocal
+from .database import create_database
+from .database import engine
---
+++ database/models.py
@@ -0,0 +1,8 @@
+"""
+Database models.
+
+Currently empty.
+Tables will be added in future modules.
+"""
+
+from database.database import Base
